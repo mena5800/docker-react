e89327736dd71e8d001a25bb1232bf3e0aee3f89
mena5800 <mr.safwat55@gmail.com>
2024-04-26 16:18:31 +0300

    edit text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hi there mina welcome . go 
+          hi there
         </p>
         <a
           className="App-link"
